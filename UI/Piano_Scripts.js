71d60dfe45a66d65b8a4880614c938e8d41f480b
Christopher Wright <valtera45@gmail.com>
2014-10-14 21:43:41 -0400

    Piano w/Tutorial and Game Mode

--- UI/Piano_Scripts.js
+++ UI/Piano_Scripts.js
@@ -1,4 +1,4 @@
- $(document).ready(function() {
+$(document).ready(function() {
  	// THESE FUNCTIONS CHANGE THE CSS CLASS OF EACH KEY HIT TO PRESS KEY DOWN
  	$(".whitekey").mousedown(function () {
  			$(this).addClass("whitekey_down");
@@ -12,20 +12,60 @@
  	$(".blackkey").mouseup(function() {
  			$(this).removeClass("blackkey_down");
  	});
- 
- 	// These functions get a reference to each key pressed and play the related sound file
-	function mousedown(soundObj) {
-
-		var audio1 = document.getElementById(soundObj);
-		audio1.play();
-
-	}
-
-	function mouseup(soundObj) {
-
-		var audio1 = document.getElementById(soundObj);
-		audio1.pause();
-		audio1.currentTime = 0;
-
-	}
+ 	 $(".whitekey_small").mousedown(function () {
+ 		$(this).addClass("whitekey_down_small");
+ 	});
+ 	$(".whitekey_small").mouseup(function() {
+ 			$(this).removeClass("whitekey_down_small");
+ 	});
+ 	$(".blackkey_small").mousedown(function () {
+ 			$(this).addClass("blackkey_down_small");
+ 	});
+ 	$(".blackkey_small").mouseup(function() {
+ 			$(this).removeClass("blackkey_down_small");
+ 	});
+ 	$(document).keyup(function () {
+ 		$("div").removeClass('whitekey_down');
+ 		$("div").removeClass('blackkey_down');
+ 	});
+ 	//THIS INITIALLY HIDES THE KEYBOARD TO DISPLAY FREE PLAY KEYBOARD AT DEFAULT
+ 	$(".small").hide();
+ 	//DISPLAYS FULL SIZE FREE PLAY KEYBOARD ON CLICK
+ 	$("#fullsize").click(function() {
+ 		$('.small').hide();
+ 		$('.large').show();
+ 	});
+ 	//DISPLAYS TUTORIAL SIZE KEYBOARD ON CLICK
+ 	$("#smallsize").click(function() {
+ 		$('.large').hide();
+ 		$('.small').show();
+ 	});
  });
+// THESE FUNCTIONS USE THE KEY PRESSED AS A REFERENCE, TAKE THE ID OF THAT KEY, AND PLAY THE RELATED SOUND FILE
+function playNote(soundObj) {
+	var audio1 = document.getElementById(soundObj);
+	audio1.play();
+	
+}
+
+function stopNote(soundObj) {
+
+	var audio1 = document.getElementById(soundObj);
+	audio1.pause();		
+	audio1.currentTime = 0;
+
+}
+// THESE FUNCTIONS CHANGE THE CLASS TO DISPLAY A KEY CHANGE ON KEY PRESS DOWN.  
+function keydown_white(key) {
+	
+	$("div#" + key).addClass('whitekey_down');
+
+}
+
+function keydown_black(key) {
+	
+	$("div#" + key).addClass('blackkey_down');
+	
+}
+
+
