d4d76a42dfcbd3ddd8ea6530c1d0fe9da1e71a90
greenelephant1 <abeglova@bu.edu>
2014-11-28 20:26:42 -0500

    get rid of changeKeys

--- UI/Levels_Services.js
+++ UI/Levels_Services.js
@@ -11,30 +11,12 @@ levels_app.service('TutorialsNavService', ['$http', function($http) {
         api_call('http://keyboard.cloudapp.net:3010/MusicService/demo', response);
     };
 
-    function api_call(api_call, response, oldKeys, newKeys) {
+    function api_call(api_call, response) {
         $http.get(api_call).
             success(function(data) {
-                if (typeof oldKeys !== 'undefined' && oldKeys.length > 0) {
-                    data = changeKeys(oldKeys, newKeys, data);
-                }
                 response(data);
             }).error(function(data,status) {
                  response([])
             });
-    }
-
-    function changeKeys(originalKeys, newKeys, arr)
-    {
-        var newArr = [];
-        for(var pos = 0; pos < arr.length; pos++)
-        {
-            var obj = arr[pos];
-            for( var key_pos = 0; key_pos < originalKeys.length; key_pos ++){
-                obj[newKeys[key_pos]] = obj[originalKeys[key_pos]];
-                delete(obj[originalKeys[key_pos]]);
-                newArr.push(obj);
-            }
-        }
-        return newArr;
     }
 }]);
